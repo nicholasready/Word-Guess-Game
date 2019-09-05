ab5fdf83427aba1f2d41ad45fed08befa9472cf0
Nicholas Ready <Ready.nicholas@gmail.com>
2019-09-04 23:11:12 -0500

    with basic HTML file, CSS, and variables for Java

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+// Create variables with different array names using each letter of the word as its item
+var tv = ["n","i","c","k","e","l","o","d","e","o","n"];
+var saying = ["w","h","a","t","e","v","e","r"];
+var devices = ["g","a","m","e","b","o","y"];
+var internet = ["s","c","r","e","e","n","n","a","m","e"];
+var show = ["f","r","i","e","n","d","s"];
+var vhs = ["b","l","o","c","k","b","u","s","t","e","r"];
+var singer = ["b","r","i","t","n","e","y"];
+
+//Create other variables that will be used in the game
+var wins = 0;
+var guessesLeft = 6;
+var words = [nickelodeon, whatever, gameboy, screenname, friends, blockbuster, britney];
+var lettersGuessed = [];
+var lettersGuessed2 = [];
+var answerDisplay = []
+
+//Create variables that hold references to the places in the HTML where we want to display things.
+var wins = document.getElementById("wins");
+var theWord = document.getElementById("theWord");
+var guesses = document.getElementById("guesses");
+var lettersGuessed = document.getElementById("lettersGuessed");
+var lettersGuessed2 = document.getElementById("lettersGuessed2");
+       
+
+        
